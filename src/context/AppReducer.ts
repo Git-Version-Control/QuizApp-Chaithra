b7112819c6a231c0288182b3dad92100e2102cb6
Chaithra <102819533+Git-Version-Control@users.noreply.github.com>
2022-05-12 20:02:46 +0530

    Add files via upload

--- src/context/AppReducer.ts
+++ src/context/AppReducer.ts
@@ -1,1 +1,25 @@
+type State = {
+  answers: any;
+};
 
+type Action = {
+  type: string;
+  payload: {
+    id: string;
+    data: any;
+  };
+};
+
+export default function appReducer(state: State, action: Action) {
+  switch (action.type) {
+    case "SET_ANSWER":
+      return {
+        answers: {
+          ...state.answers,
+          [action.payload.id]: action.payload.data,
+        },
+      };
+    default:
+      throw new Error();
+  }
+}

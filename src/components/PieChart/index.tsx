8e73dcfb5a2e5bc648d801cfd3a56f9f56cb7255
Chaithra <102819533+Git-Version-Control@users.noreply.github.com>
2022-05-13 15:11:59 +0530

    Create index.tsx

---
+++ src/components/PieChart/index.tsx
@@ -0,0 +1,58 @@
+import Box from "@mui/material/Box";
+import questions from "../../questions.json";
+import { useNavigate } from "react-router-dom";
+
+const Circle: any = ({ id, answered = false }: any) => {
+  
+  return (
+    <Box
+      component="span"
+      sx={{
+        //bgcolor: 'gray',
+        bgcolor: answered ? "green" : "gray",
+        borderRadius: "50%",
+        display: "flex",
+        color: "white",
+        width: 40,
+        height: 40,
+        justifyContent: "center",
+        alignItems: "center",
+        boxShadow: "2px 2px 10px 1px gray;",
+      }}
+    >
+
+      {id}
+    </Box>
+
+  )
+}
+
+const QuestionHighlighting = ({ answered, current }: any) => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        top: "80px",
+        right: 0,
+        left: 0,
+        justifyContent: "space-around",
+        position: "absolute",
+        backgroundColor: "yellow"
+
+      }}
+    >
+
+      {questions.map((question) => {
+        return (
+          <Circle
+            key={question.id}
+            id={question.id}
+          //answered={answered}
+          />
+        );
+      })}
+    </Box>
+  );
+};
+
+export default QuestionHighlighting;
